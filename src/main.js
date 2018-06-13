4d64057134d54103e965e74e61c96b6a4d0e84de
Abner Suniga <abner.suniga@hotmail.com>
2018-06-13 14:24:37 -0300

    Circle Fix Rect OK

--- src/main.js
+++ src/main.js
@@ -14,18 +14,24 @@ function draw() {
   strokeWeight(3);
   rect(0, 0, 639, 479);
 
-  // When the user hasn't choose a last point
+  // When the user hasn't chosen the last point
   if(state.drawingMode == "line" && state.clicks.length > 1) {
     line(state.clicks[state.clicks.length-1].x, state.clicks[state.clicks.length-1].y, mouseX, mouseY);
   }
-
   if(state.drawingMode == "circle" && state.clicks.length > 1) {
-    let x = state.clicks[state.clicks.length-1].x
-    let y = state.clicks[state.clicks.length-1].y
+    let x = state.clicks[state.clicks.length-1].x;
+    let y = state.clicks[state.clicks.length-1].y;
     let distance = dist(x, y, mouseX, mouseY);
-    ellipse(x, y, distance, distance);
+    ellipse(x, y, distance*2, distance*2);
   }
-  
+  if(state.drawingMode == "rect" && state.clicks.length > 1) {
+    let x = state.clicks[state.clicks.length-1].x;
+    let y = state.clicks[state.clicks.length-1].y;
+    let width = mouseX - x;
+    let height = mouseY - y;
+    rect(x, y, width, height);
+  }
+
   for(object of state.objects) {
     object.show();
   }
@@ -50,7 +56,20 @@ class Circle {
 
   show() {
     let distance = dist(this.point1.x, this.point1.y, this.point2.x, this.point2.y)
-    ellipse(this.point2.x, this.point2.y, distance, distance);
+    ellipse(this.point2.x, this.point2.y, distance*2, distance*2);
+  }
+}
+
+class Rect {
+  constructor(point1, point2) {
+    this.point1 = point1;
+    this.point2 = point2;
+  }
+
+  show() {
+    let width = this.point2.x - this.point1.x;
+    let height = this.point2.y - this.point1.y;
+    rect(this.point1.x, this.point1.y, width, height);
   }
 }
 
@@ -68,12 +87,23 @@ function createCircle() {
   state.objects.push(circle);
 }
 
+function createRect() {
+  const point1 = state.clicks.pop();
+  const point2 = state.clicks.pop();
+  const rect = new Rect(point1, point2)
+  state.objects.push(rect);
+}
+
 function drawLineMode() {
   state.drawingMode = "line";
 }
 
 function drawCircleMode() {
   state.drawingMode = "circle";
+}
+
+function drawRectMode() {
+  state.drawingMode = "rect";
 }
 
 function mouseClicked() {
@@ -109,6 +139,25 @@ function mouseClicked() {
       });
       
       createCircle();
+      // Reset
+      state.clicks = []
+      state.drawingMode = "none";
+    }
+  }
+
+  if(state.drawingMode == "rect") {
+    if(state.clicks.length < 2) {
+      state.clicks.push({
+        x: mouseX,
+        y: mouseY
+      });
+    } else {
+      state.clicks.push({
+        x: mouseX,
+        y: mouseY
+      });
+      
+      createRect();
       // Reset
       state.clicks = []
       state.drawingMode = "none";
