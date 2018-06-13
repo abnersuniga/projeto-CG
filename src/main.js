efb24f1cb68e1285214cca5fe8c04e09205120b1
Abner Suniga <abner.suniga@hotmail.com>
2018-06-13 14:03:40 -0300

    Circulo implementado

--- src/main.js
+++ src/main.js
@@ -14,11 +14,17 @@ function draw() {
   strokeWeight(3);
   rect(0, 0, 639, 479);
 
-  // While a user did
+  // When the user hasn't choose a last point
   if(state.drawingMode == "line" && state.clicks.length > 1) {
     line(state.clicks[state.clicks.length-1].x, state.clicks[state.clicks.length-1].y, mouseX, mouseY);
   }
 
+  if(state.drawingMode == "circle" && state.clicks.length > 1) {
+    let x = state.clicks[state.clicks.length-1].x
+    let y = state.clicks[state.clicks.length-1].y
+    let distance = dist(x, y, mouseX, mouseY);
+    ellipse(x, y, distance, distance);
+  }
   
   for(object of state.objects) {
     object.show();
@@ -36,17 +42,38 @@ class Line {
   }
 }
 
+class Circle {
+  constructor(point1, point2) {
+    this.point1 = point1;
+    this.point2 = point2;
+  }
+
+  show() {
+    let distance = dist(this.point1.x, this.point1.y, this.point2.x, this.point2.y)
+    ellipse(this.point2.x, this.point2.y, distance, distance);
+  }
+}
+
 function createLine() {
   const point1 = state.clicks.pop();
   const point2 = state.clicks.pop();
   const line = new Line(point1, point2)
   state.objects.push(line);
+}
 
-  console.log(state.objects);
+function createCircle() {
+  const point1 = state.clicks.pop();
+  const point2 = state.clicks.pop();
+  const circle = new Circle(point1, point2)
+  state.objects.push(circle);
 }
 
 function drawLineMode() {
   state.drawingMode = "line";
+}
+
+function drawCircleMode() {
+  state.drawingMode = "circle";
 }
 
 function mouseClicked() {
@@ -63,6 +90,25 @@ function mouseClicked() {
       });
       
       createLine();
+      // Reset
+      state.clicks = []
+      state.drawingMode = "none";
+    }
+  }
+
+  if(state.drawingMode == "circle") {
+    if(state.clicks.length < 2) {
+      state.clicks.push({
+        x: mouseX,
+        y: mouseY
+      });
+    } else {
+      state.clicks.push({
+        x: mouseX,
+        y: mouseY
+      });
+      
+      createCircle();
       // Reset
       state.clicks = []
       state.drawingMode = "none";
